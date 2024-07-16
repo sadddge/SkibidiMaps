08c06e2ca0e9a836ddf5aabd27a1f282517dbc8c
sadddge <m.sanhueza22@ufromail.cl>
2024-07-16 00:18:47 -0400

    listado de imagenes y mapa

--- mapillary.js
+++ mapillary.js
@@ -5,24 +5,74 @@ container.style.width = '1200px';
 container.style.height = '900px';
 document.body.appendChild(container);
 
-const viewer = new Viewer({
-    accessToken: 'MLY|7783857768335335|3c8731e3c9d763554e7d2c4519ee1858',
-    container,
-    imageId: '154753879917602',
-});
-
-async function fetchImageLocation(imageId, accessToken) {
-    const response = await fetch(`https://graph.mapillary.com/${imageId}?fields=id,geometry&access_token=${accessToken}`);
-    const data = await response.json();
-    return data.geometry.coordinates;
-}
-
 const imageId = '154753879917602';
 const accessToken = 'MLY|7783857768335335|3c8731e3c9d763554e7d2c4519ee1858';
+let bboxes = ["33.446,-17.649,68.129,47.482"]
+let images = [];
 
-fetchImageLocation(imageId, accessToken)
-    .then(coordinates => {
-        const locationInfoDiv = document.getElementById('location-info');
-        locationInfoDiv.innerHTML = `Latitud: ${coordinates[1]}, Longitud: ${coordinates[0]}`;
-    })
-    .catch(error => console.error('Error fetching image location:', error));
+let viewer = null;
+
+function showImage(imageId) {
+    if (viewer) {
+        viewer.remove();
+    }
+    viewer = new Viewer({
+        container,
+        imageId,
+        accessToken
+    });
+}
+
+
+
+
+
+
+async function fetchAllImages() {
+    for (const bbox of bboxes) {
+        const imagesInBbox = await fetch(`https://graph.mapillary.com/images?access_token=${accessToken}&fields=id,geometry&limit=200&bbox=${bbox}`)
+        const data = await imagesInBbox.json();
+        for (const image of data.data) {
+            const info = {
+                id: image.id,
+                coordinates: image.geometry.coordinates
+            };
+            images.push(info);
+        }
+    }
+}
+
+fetchAllImages().then(main);
+
+function switchImage(index) {
+    if (index < 0) {
+        index = images.length - 1;
+    } else if (index >= images.length) {
+        index = 0;
+    }
+    showImage(images[index].id);
+    setTimeout(() => switchImage(index + 1), 4000);
+}
+
+function main() {
+    const map = L.map('map').setView([0, 0], 2); // Coordenadas centradas en el mundo y zoom inicial
+
+    // Añadir capa de tiles de OpenStreetMap
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    // Manejar el clic en el mapa
+    map.on('click', function(e) {
+        const { lat, lng } = e.latlng;
+        L.marker([lat, lng]).addTo(map)
+            .bindPopup(`You clicked the map at ${lat.toFixed(2)}, ${lng.toFixed(2)}`)
+            .openPopup();
+    });
+}
+
+
+
+
+
+
